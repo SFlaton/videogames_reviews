2c0033031c14f297b44603ac4e7289683fd2559a
lolopaluza <upsonmanoah@gmail.com>
2016-04-29 14:44:46 +0200

    syntax fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,9 +2,9 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
-  
+
     respond_to do |f|
-      f.html { render }
+      f.html 
       f.json { render json: { category: @category, games: @games } }
     end
   end
